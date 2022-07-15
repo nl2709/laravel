6651024a4125fe857826cc96ecd439e7937cb20e
nl2709 <developer@nick-lorenz.de>
2022-07-15 11:18:51 +0200

    Add

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Brücker Manufraktur</title>
+    <title>Title</title>
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="shortcut icon" href="assets/shortcut_icon.png">
@@ -18,37 +18,6 @@
 
 
 <body>
-
-<nav>
-
-        <div class="row">
-            <div class="col-2">
-                <img  class="img-fluid" src="assets\logo.png">
-            </div>
-            <div class="col-2"></div>
-            <div class="col-1">
-                <a class="nav-item nav-link white " href="#">Home</a>
-            </div>
-            <div class="col-1">
-                <a class="nav-item nav-link white " href="#">Termine</a>
-            </div>
-            <div class="col-1">
-                <a class="nav-item nav-link white " href="#">Über uns</a>
-            </div>
-            <div class="col-1">
-                <a class="nav-item nav-link white " href="#">Kontakt</a>
-            </div>
-            <div class="col-2"></div>
-            <div class="col-2">
-                <button type="button" class="booking"><a>Jetzt Buchen</a></button>
-            </div>
-        </div>
-
-
-
-
-</nav>
-
 
     <div class="row">
         <div class="col-12 bg-beige">
